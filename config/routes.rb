6cac0ce69a190efb0703c0dab9219742d962e955
Maiyoj <valentinemaiyo1995@gmail.com>
2022-10-23 13:17:55 +0300

    [setup] user authentication and add authorization helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :stocks
   resources :products
   resources :categories
+  resources :users
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_192201) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_23_054413) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -58,6 +58,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_192201) do
   create_table "stocks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "product_id"
     t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
