0eec112735f5df5dfa51a0f63cf463596353f841
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-24 22:50:46 +0300

    fetching produts data

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -31,40 +31,33 @@ function Products() {
     },
   }));
 
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
+  const [ product, setProducts ] = useState([])
+  const url = 'http://localhost:3000/products'
 
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
-  ];
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data)
+    })
+  },[])
 
-  // const [product, setProduct] = useState([]);
 
-  // const url = "http://localhost:3000/products";
-  // useEffect(() => {
-  //   fetch(url)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //       setProduct(data);
-  //     });
-  // });
 
-  // function deleteProduct(id) {
-  //   fetch(`http://localhost:3000/products/${id}`, {
-  //     method: "DELETE",
-  //   })
-  //     .then((r) => r.json())
-  //     .then(() => {
-  //       const goThru = product.filter((product) => product.id !== id);
-  //       setProduct(goThru);
-  //     });
+  // function createData(name, calories, fat, carbs, protein) {
+  //   return { name, calories, fat, carbs, protein };
   // }
+
+  // const rows = [
+  //   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  //   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  //   createData("Eclair", 262, 16.0, 24, 6.0),
+  //   createData("Cupcake", 305, 3.7, 67, 4.3),
+  //   createData("Gingerbread", 356, 16.0, 49, 3.9),
+  // ];
+
+
 
   return (
     <>
