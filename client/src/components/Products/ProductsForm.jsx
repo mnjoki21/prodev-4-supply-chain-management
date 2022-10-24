40d6a5a18007792b080db0b0b2e3259553d7fcec
Fridah Omari <nginaomari@gmail.com>
2022-10-24 11:35:12 +0300

    created products form

--- client/src/components/Products/ProductsForm.jsx
+++ client/src/components/Products/ProductsForm.jsx
@@ -1,1 +1,15 @@
 import TextField from "@mui/material/TextField";
+
+
+
+
+
+function ProductsForm() {
+    const [drop, onDrop] = useState("");
+  
+    return (
+      <div className="products_form">
+        <Paper className="card" elevation={2}>
+          <div style={{ width: "90%" }}>
+            <h2 style={{ textAlign: "left" }}>Add Item</h2>
+          </div>
