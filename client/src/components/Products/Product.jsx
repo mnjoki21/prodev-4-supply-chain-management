32a69a3628ee47804db7e01e0991ff9b6ea16751
Fridah Omari <nginaomari@gmail.com>
2022-10-24 16:41:29 +0300

    Revert "Solve merge conflict"
    This reverts commit 86004209a864ed8025f5efd1cb122206797564a1.

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -6,7 +6,6 @@ export default function Product() {
   return (
     <Box>
    <Typography paragraph>
-    hghgfhgfh
    Hey Valentine,Welcome to products
    Hey Valentine,Welcome to products
    Hey Valentine,Welcome to products
