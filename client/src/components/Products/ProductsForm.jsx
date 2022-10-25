5f14bf2b8b52cd8f89ce471305ff29f6020d8a7e
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-25 14:28:25 +0300

    changes

--- client/src/components/Products/ProductsForm.jsx
+++ client/src/components/Products/ProductsForm.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Paper from "@mui/material/Paper";
-import { useState } from "react";
-import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
-
 
 function ProductsForm() {
-    const [drop, setDrop] = useState('')
   return (
     <div>ProductsForm</div>
   )
