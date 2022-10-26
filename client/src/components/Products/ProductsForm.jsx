a00e17ce39ffe84af7c872ba9233080f6eca59e3
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-26 09:23:22 +0300

    milestone created: fixing products

--- client/src/components/Products/ProductsForm.jsx
+++ client/src/components/Products/ProductsForm.jsx
@@ -1,9 +1,134 @@
-import React from 'react'
+import {Alert,Box,FormControl,Button,FormHelperText,TextField, Grid,} from "@mui/material";
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+
 
 function ProductsForm() {
+  const [ formData, setFormData ] = useState({
+    id:'',
+    name: '',
+    description: '',
+  })
+  const url = "http://lovalhost:3000/products"
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(url, {
+      method: "POST",
+      Headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    setFormData({
+      id: '',
+      name: '',
+      description:''
+    })
+    console.log(formData)
+  }
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.taget.name]: e.target.value
+    })
+  }
+
+  
+
   return (
-    <div>ProductsForm</div>
-  )
+    <>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Box>
+          <Card sx={{ minWidth: 400 }}>
+            <main>
+              <form>
+                <p
+                  style={{
+                    fontWeight: "bolder",
+                    fontSize: 40,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  Form
+                </p>
+
+                <Box sx={{ "& .MuiTextField-root": { m: 1, width: "35ch" } }}>
+                  <div>
+                    <FormControl>
+                      <FormHelperText id="my-helper-text">
+                        Enter your Name here
+                      </FormHelperText>
+                      <TextField
+                        type="text"
+                        variant="outlined"
+                        label="Name"
+                  
+                        autoComplete="on"
+                        value={name}
+                        sx={{ minWidth: 400 }}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </FormControl>
+                    <br />
+                    <FormControl>
+                      <FormHelperText >
+                        Enter description here
+                      </FormHelperText>
+                      <TextField
+                        type="text"
+                        label="text"
+                        sx={{ minWidth: 400 }}
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                      />
+                    </FormControl>
+                  </div>
+                </Box>
+
+                <br />
+                <br />
+                <div>
+                  <Box textAlign="center">
+                    <Button
+                      variant="contained"
+                      type="submit"
+                      style={{ fontSize: 16 }}
+                    >
+                      Submit
+                    </Button>
+                  </Box>
+                  <br />
+
+                  <div>
+                    {errors.map((err) => (
+                      <>
+                        <Alert
+                          key={err}
+                          severity="error"
+                          sx={{ width: "100%" }}
+                        >
+                          {err}
+                        </Alert>
+                      </>
+                    ))}
+                  </div>
+                </div>
+              </form>
+            </main>
+          </Card>
+        </Box>
+      </Grid>
+    </>
+  );
 }
 
 export default ProductsForm
