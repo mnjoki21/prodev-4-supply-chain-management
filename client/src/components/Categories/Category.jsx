7da3f291835c19e64c57227c7e7cbecb4d7a49cf
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-10-29 12:37:13 +0300

    Add: create and read on categories

--- client/src/components/Categories/Category.jsx
+++ client/src/components/Categories/Category.jsx
@@ -12,7 +12,10 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -20,7 +23,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+    fontSize: 30,
   },
 }));
 
@@ -31,23 +34,28 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   // hide last border
   '&:last-child td, &:last-child th': {
     border: 0,
+
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+
+
+
+
+
+
 
 
 export default function Category() {
+
+   const [category, setCategory]= React.useState([])
+
+
+    const categories = fetch("http://localhost:3000/categories")
+                          .then((r) => r.json())
+                          .then(response => setCategory(response));  
+
   return (
     
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
@@ -60,33 +68,33 @@ export default function Category() {
       <Table sx={{ minWidth: 1000, ml:10 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Actions&nbsp;(g)</StyledTableCell>
+            <StyledTableCell>CATEGORY</StyledTableCell>
+            <StyledTableCell align="right" >Delete</StyledTableCell>
+            <StyledTableCell align="right">Edit</StyledTableCell>
+      
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
+
+
+        {category.map((row) => (
+        
+            <StyledTableRow key={row.id} >
               <StyledTableCell component="th" scope="row">
-                {row.name}
+              {row.name}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-              <StyledTableCell align="right"><button>Edit</button></StyledTableCell>
+                        <StyledTableCell align="right"><Button variant="contained" color="error" startIcon={<DeleteIcon />}>
+            Delete
+          </Button></StyledTableCell>
+              <StyledTableCell align="right"><Button variant="outlined" startIcon={<EditIcon />}>
+        Edit
+      </Button>
+</StyledTableCell>
               
             </StyledTableRow>
           ))}
         </TableBody>
-      </Table>
-  
-
-                
+      </Table>     
               </Grid>
               </Grid>
     
