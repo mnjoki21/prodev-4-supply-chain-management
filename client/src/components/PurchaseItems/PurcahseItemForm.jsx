9a3f27208243e0017b449de56c82c9919a78337c
Maiyoj <valentinemaiyo1995@gmail.com>
2022-10-29 19:44:52 +0300

    [update] purchase item invoive value

---
+++ client/src/components/PurchaseItems/PurcahseItemForm.jsx
@@ -0,0 +1,27 @@
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { Fragment, useEffect, useState } from "react";
+function PurchaseItemForm(){
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+    HELLO USER
+    <Grid container spacing={10} >
+      {/* Chart */}
+      <Grid item xs={12} md={8} lg={9}>
+        <Paper
+          sx={{
+            p: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            height: 240,
+          }}
+        >
+        </Paper>
+      </Grid>
+      </Grid>
+      </Container>
+
+}
+export default PurchaseItemForm
