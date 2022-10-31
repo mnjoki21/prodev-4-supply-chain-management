4152ecb4accc10d696629df63e64b1ab21bf4099
Fridah Omari <nginaomari@gmail.com>
2022-10-31 10:00:26 +0300

    imported material ui dependencies

--- client/src/components/Vendors/VendorsForm.jsx
+++ client/src/components/Vendors/VendorsForm.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import { Alert, Box, FormControl, Button,FormHelperText, TextField, Grid, unstable_composeClasses } from '@mui/material';
+
 
 export default function VendorsForm() {
   return (
