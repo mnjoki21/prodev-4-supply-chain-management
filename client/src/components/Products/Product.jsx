c27a967c5ee0faee4f526e5c60b576e7d017ecae
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-31 11:44:16 +0300

    fix: merge conflict on products

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -1,3 +1,9 @@
+import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import ProductsForm from "./ProductsForm";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
@@ -6,114 +12,96 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { useEffect, useState } from "react";
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import ProductsForm from "./ProductsForm";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { useEffect, useState } from "react";
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 30,
+  },
+}));
 
-function Products() {
-  const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.primary.main,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    "&:last-child td, &:last-child th": {
-      border: 0,
-    },
-  }));
+export default function Product() {
+  //  const [category, setCategory]= React.useState([])
+  //  console.log(category)
 
-  const [products, setProducts] = useState([]);
+  //  useEffect(() => {
+  //   fetch("http://localhost:3000/categories")
+  //     .then((r) => r.json())
+  //     .then(data =>setCategory(data));
+  // }, []);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/products")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+  // function handleDelete(id) {
+  //   console.log(id)
+  //   fetch(`http://localhost:3000/categories/${id}`, {
+  //     method: "DELETE",
+  //   })
+  //     .then((r) => r.json())
+  //     .then(() => {
+  //       deleteEvent(id)
+  //       // const deletion = category.filter((item) => item.id !== id);
+  //       // setCategory(deletion);
+  //     });
+  // }
 
-        setProducts(data);
-      });
-  }, []);
-
-  function deleteProduct(id) {
-    fetch(`http://localhost:3000/products/${id}`,
-    {
-      method: 'DELETE'
-    }
-    )
-      .then((r) => r.json())
-      .then((data) => {
-        const goThru = data.filter(
-          (dataItem) => dataItem.id !== id
-        );
-        console.log(goThru)
-        setProducts(goThru)
-    })
-  }
+  // function deleteEvent(id) {
+  //   const updatedEvents = category.filter((one) => one.id !== id);
+  //   setCategory(updatedEvents);
+  // }
 
   return (
-    <>
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={8} lg={9}>
-            <ProductsForm />
-
-            <TableContainer component={Paper}>
-              <Table
-                sx={{ minWidth: 1350, ml: 10 }}
-                aria-label="customized table"
-              >
-                <TableHead>
-                  <TableRow>
-                    <StyledTableCell>Id</StyledTableCell>
-                    <StyledTableCell align="right">Name</StyledTableCell>
-                    <StyledTableCell align="right">Description</StyledTableCell>
-                    <StyledTableCell align="right">Category id</StyledTableCell>
-                    <StyledTableCell align="right">Threshold</StyledTableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {products.map((item) => {
-                    return (
-                      <>
-                        <StyledTableRow key={item.id}>
-                          <StyledTableCell component="th" scope="row">
-                            {item.name}
-                          </StyledTableCell>
-                          <StyledTableCell align="right">
-                            {item.description}
-                          </StyledTableCell>
-                          <StyledTableCell align="right">
-                            {item.threshold}
-                          </StyledTableCell>
-
-                          {/* <StyledTableCell align="right">{row.carbs}</StyledTableCell>  */}
-                          {/* <StyledTableCell align="right">{row.protein}</StyledTableCell> */}
-                        </StyledTableRow>
-                      
-                      </>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Grid>
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <ProductsForm />
+      <Grid container spacing={3}>
+        {/* Chart */}
+        <Grid item xs={12} md={8} lg={9}>
+          <Table sx={{ minWidth: 1000, ml: 10 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>CATEGORY</StyledTableCell>
+                <StyledTableCell align="right">Delete</StyledTableCell>
+                <StyledTableCell align="right">Edit</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* {category.map((row) => 
+        
+        (
+            <StyledTableRow key={row.id} >
+              <StyledTableCell component="th" scope="row">
+              {row.name}
+              </StyledTableCell>
+             <StyledTableCell align="right"><Button onClick={()=>handleDelete(row.id)} variant="contained" color="error" startIcon={<DeleteIcon />}>
+            Delete
+          </Button></StyledTableCell>
+              <StyledTableCell align="right"><Button variant="outlined" startIcon={<EditIcon />}>
+        Edit
+      </Button>
+</StyledTableCell>
+              
+            </StyledTableRow>
+          ))} */}
+            </TableBody>
+          </Table>
         </Grid>
-      </Container>
-    </>
+      </Grid>
+    </Container>
   );
 }
-
-export default Products;
--- client/src/components/Products/ProductsForm.jsx
+++ client/src/components/Products/ProductsForm.jsx
@@ -1,13 +1,19 @@
+<<<<<<< HEAD
 import {Alert,Box,FormControl,Button,FormHelperText,TextField, Grid,} from "@mui/material";
 import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
+=======
+import React from 'react'
+import { Alert, Box, FormControl,Card, Button,FormHelperText, TextField, Grid, unstable_composeClasses } from '@mui/material';
+>>>>>>> 89b4406a (Add: product Form)
 
 
 
 function ProductsForm() {
 
+<<<<<<< HEAD
   // const [ product, setProduct ] = useState([]);
   const [ formData, setFormData ] = useState({
     name: '',
@@ -202,6 +208,104 @@ function ProductsForm() {
       </>
     </>
   );
+=======
+
+
+
+
+
+  return (
+    <div>
+        <Grid  container direction="row" alignItems="center" justifyContent="center" >
+      <Box    >
+      <Card sx={{ minWidth: 400 }}>
+        <main >
+        <form  >
+          <p style={{fontWeight: "bolder", fontSize: 40 ,alignItems:"center", justifyContent:"center",textAlign:'center'}}>Add Product</p>
+          
+          <Box sx={{'& .MuiTextField-root': { m: 1, width: '35ch' },}}>
+          
+            <div>
+              
+              <FormControl>
+              {/* <FormHelperText id="my-helper-text">Category</FormHelperText> */}
+                <TextField 
+                type="text"
+                variant="outlined"
+                label="Enter Product Name"
+                id="name"
+                autoComplete="on"
+                value={name}
+                sx={{ minWidth: 400 }}
+                onChange={(e) => setName(e.target.value)} 
+                />
+               
+              </FormControl>
+              <br />
+              <FormControl>
+              {/* <FormHelperText id="my-helper-text">Category</FormHelperText> */}
+                <TextField 
+                type="text"
+                variant="outlined"
+                label="Enter Product Description"
+                id="name"
+                autoComplete="on"
+                value={name}
+                sx={{ minWidth: 400 }}
+                onChange={(e) => setName(e.target.value)} 
+                />
+               
+              </FormControl>
+              <br />
+              <FormControl>
+              {/* <FormHelperText id="my-helper-text">Category</FormHelperText> */}
+                <TextField 
+                type="text"
+                variant="outlined"
+                label="Select Product category"
+                id="name"
+                autoComplete="on"
+                value={name}
+                sx={{ minWidth: 400 }}
+                onChange={(e) => setName(e.target.value)} 
+                />
+               
+              </FormControl>
+            </div>
+          </Box>
+          
+          <br />
+          <br />
+          <div>
+            
+                <Box textAlign='center'>
+      <Button variant='contained'  type="submit" style={{fontSize: 16 }}>
+        Submit
+      </Button>
+     </Box>
+              <br />
+          
+            <div>
+              {/* {errors.map((err) => (
+              <>
+                <Alert key={err} severity="error" sx={{ width: '100%' }}>
+                  {err}
+                </Alert>
+              </>
+              ))}  */}
+            </div>       
+          </div>
+          </form>
+        </main>
+        </Card>
+      </Box>
+      </Grid>
+
+
+
+    </div>
+  )
+>>>>>>> 89b4406a (Add: product Form)
 }
 
 export default ProductsForm
