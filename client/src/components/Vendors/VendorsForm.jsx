f173cc06bb773e06a50908488b71b639b25e1a64
Fridah Omari <nginaomari@gmail.com>
2022-10-31 11:52:46 +0300

    creating states for the fields

--- client/src/components/Vendors/VendorsForm.jsx
+++ client/src/components/Vendors/VendorsForm.jsx
@@ -1,47 +1,125 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Card from '@mui/material/Card';
 import { Alert, Box, FormControl, Button,FormHelperText, TextField, Grid, unstable_composeClasses } from '@mui/material';
 
 
-export default function VendorsForm() {
-  return (
-    <div>
-        <FormControl>
-            <TextField 
-            type="text"
-            variant="outlined"
-            label="Enter Name"
-            id="name"
-            autoComplete="on"
-            />
 
-            <TextField 
-            type="text"
-            variant="outlined"
-            label="Enter Email"
-            id="name"
-            autoComplete="on"
-            />
+const VendorsForm = () => {
 
-            <TextField 
-            type="text"
-            variant="outlined"
-            label="Enter Address"
-            id="name"
-            autoComplete="on"
-            />
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState("");
+    const [errors, setErrors] = useState([]);
 
-            <TextField 
-            type="text"
-            variant="outlined"
-            label="Enter Phone Number"
-            id="name"
-            autoComplete="on"
-            />
-
+   
+  
     
-        </FormControl>
-        
-    </div>
-  )
-}
+  
+    return (
+      <>
+      <Grid  container direction="row" alignItems="center" justifyContent="center" >
+        <Box    >
+        <Card sx={{ minWidth: 400 }}>
+          <main >
+          <form  >
+            <p style={{fontWeight: "bolder", fontSize: 40 ,alignItems:"center", justifyContent:"center",textAlign:'center'}}>Form</p>
+            
+            <Box sx={{'& .MuiTextField-root': { m: 1, width: '35ch' },}}>
+            
+              <div>
+                
+                <FormControl>
+                <FormHelperText id="my-helper-text">Enter your Name</FormHelperText>
+                  <TextField 
+                  type="text"
+                  variant="outlined"
+                  label="Full Name"
+                  id="name"
+                  autoComplete="on"
+                  value={name}
+                  sx={{ minWidth: 400 }}
+                  onChange={(e) => setName(e.target.value)} 
+                  />            
+                </FormControl>
+
+                <FormControl>
+                <FormHelperText id="my-helper-text">Enter your Email</FormHelperText>
+                  <TextField 
+                  type="email"
+                  variant="outlined"
+                  label="Email"
+                  id="email"
+                  autoComplete="on"
+                  value={email}
+                  sx={{ minWidth: 400 }}
+                  onChange={(e) => setEmail(e.target.value)} 
+                  />            
+                </FormControl>
+
+
+                <FormControl>
+                <FormHelperText id="my-helper-text">Enter your Address</FormHelperText>
+                  <TextField 
+                  type="text"
+                  variant="outlined"
+                  label="Address"
+                  id="address"
+                  autoComplete="on"
+                  value={address}
+                  sx={{ minWidth: 400 }}
+                  onChange={(e) => setAddress(e.target.value)} 
+                  />            
+                </FormControl>
+                <br />
+                <FormControl>
+                <FormHelperText id="my-helper-text">Enter your Phone Number</FormHelperText>
+                  <TextField 
+                  type="integer"
+                  label="Phone Number"
+                  id="phone_number"
+                  sx={{ minWidth: 400 }}
+                  autoComplete="on"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  />
+                  
+                </FormControl>
+
+              </div>
+            </Box>
+            
+            <br />
+            <br />
+            <div>
+              
+                  <Box textAlign='center'>
+        <Button variant='contained' type="submit" style={{fontSize: 16 }}>
+          Submit
+        </Button>
+       </Box>
+                <br />
+            
+              <div>
+                {errors.map((err) => (
+                <>
+                  <Alert key={err} severity="error" sx={{ width: '100%' }}>
+                    {err}
+                  </Alert>
+                </>
+                ))} 
+              </div>       
+            </div>
+            </form>
+          </main>
+          </Card>
+        </Box>
+        </Grid>
+      </>
+    )
+  }
+  
+  export default VendorsForm
+
+
+
