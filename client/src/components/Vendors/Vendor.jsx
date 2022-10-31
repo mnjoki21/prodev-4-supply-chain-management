09063ce532996b1985f7decbb5aa8a6f6940ad48
Fridah Omari <nginaomari@gmail.com>
2022-10-31 12:02:33 +0300

    Adds: Vendors form

--- client/src/components/Vendors/Vendor.jsx
+++ client/src/components/Vendors/Vendor.jsx
@@ -1,12 +1,12 @@
 import {Box} from "@mui/material";
 import Typography from "@mui/material/Typography";
+import VendorsForm from "./VendorsForm";
 export default function Vendor() {
   return (
     <Box>
+      <VendorsForm />
       <Typography paragraph>
-        Hey Valentine,Welcome to Vendors
-        Hey Valentine,Welcome to Vendors
-        Hey Valentine,Welcome to Vendors
+       
       </Typography>
     </Box>
   )
