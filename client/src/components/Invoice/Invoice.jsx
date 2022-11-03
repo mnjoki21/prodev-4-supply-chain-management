6e2a37c9aa5715a05db593fe651e470b07ea8fda
Maiyoj <valentinemaiyo1995@gmail.com>
2022-11-03 21:55:25 +0300

    [update] invoice to add items successfully

--- client/src/components/Invoice/Invoice.jsx
+++ client/src/components/Invoice/Invoice.jsx
@@ -12,7 +12,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import {Button} from '@mui/material';
 import {useState, useEffect} from "react";
-import PurchaseOrderForm from "./PurchaseOrderForm";
+import InvoiceForm from "./InvoiceForm";
 
 const StyledTableCell = styled(TableCell)(({theme}) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -34,24 +34,22 @@ const StyledTableRow = styled(TableRow)(({theme}) => ({
   }
 }));
 
-export default function PurchaseOrder() {
+export default function Invoice() {
   const [isAdding,
     setIsAdding] = useState(false);
-  const [orders,
-    setOrders] = useState([])
+  const [invoices,
+    setInvoices] = useState([])
   useEffect(() => {
-    fetch("http://localhost:3000/purchaseorders").then((r) => r.json()).then((orders) => {
-      setOrders(orders)
+    fetch("http://localhost:3000/invoices").then((r) => r.json()).then((invoices) => {
+      setInvoices(invoices)
     })
   }, [])
 
-  function getOrders(newOrdersReceived) {
-    const updateOrders = [
-      ...orders,
-      newOrdersReceived
-    ];
-    setOrders(updateOrders);
+  function getOrders(newInvoices) {
+    const updateItems = [...invoices, newInvoices];
+    setInvoices(updateItems);
   }
+
 
   return (
 
@@ -59,10 +57,8 @@ export default function PurchaseOrder() {
       mt: 4,
       mb: 4
     }}>
-      <Typography gutterBotton variant="h4" sx={{
-        ml: 50
-      }}>
-        Purchase Orders
+       <Typography variant="h4"  sx={{ml:50}}gutterBottom>
+        Invoices
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} md={8} lg={9}>
@@ -71,41 +67,45 @@ export default function PurchaseOrder() {
             type="submit"
             sx={{
             mb:4,
-            ml: 6
+            ml:6
           }}
-            onClick={() => setIsAdding((isAdding) => !isAdding)}>Add PurchaseOrders</Button>
+            onClick={() => setIsAdding((isAdding) => !isAdding)}>Add Invoice</Button>
 
           {isAdding
-            ? <PurchaseOrderForm getOrders={getOrders}/>
+            ? < InvoiceForm getOrders={getOrders}/>
             : null}
 
           <Table
             sx={{
             minWidth: 1000,
-            mt:10,
-            ml: 10
+            ml: 10,
+            mt: 15
           }}
             aria-label="customized table">
             <TableHead>
               <TableRow>
                 <StyledTableCell>Product</StyledTableCell>
-                <StyledTableCell align="right">Vendor</StyledTableCell>
-                <StyledTableCell align="right">Number</StyledTableCell>
-                <StyledTableCell align="right">Quantity</StyledTableCell>
-                <StyledTableCell align="right">Amount</StyledTableCell>
+                <StyledTableCell align="right">PO Number</StyledTableCell>
+                <StyledTableCell align="right">AccountName</StyledTableCell>
+                 <StyledTableCell align="right">Quantity</StyledTableCell>
+                  <StyledTableCell align="right">Amount</StyledTableCell>
+                   <StyledTableCell align="right">Vendor</StyledTableCell>
                 <StyledTableCell align="right">Actions</StyledTableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {orders.map((item) => (
-                <StyledTableRow key={item.id}>
-                  <StyledTableCell component="th" scope="row">
-                    {item.product.name}
+              {invoices.map((invoice) => (
+                <StyledTableRow key={invoice.id}>
+                   <StyledTableCell component="th" scope="row">
+                    {invoice.product.name}
                   </StyledTableCell>
-                  <StyledTableCell align="right">{item.vendor.name}</StyledTableCell>
-                  <StyledTableCell align="right">{item.number}</StyledTableCell>
-                  <StyledTableCell align="right">{item.quantity}</StyledTableCell>
-                  <StyledTableCell align="right">{item.amount}</StyledTableCell>
+                  <StyledTableCell align="right">
+                    {invoice.purchaseorder.number}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{invoice.accountname}</StyledTableCell>
+                  <StyledTableCell align="right">{invoice.quantity}</StyledTableCell>
+                  <StyledTableCell align="right">{invoice.amount}</StyledTableCell>
+                    <StyledTableCell align="right">{invoice.vendor.name}</StyledTableCell>
                   <StyledTableCell align="right">
                     <Button variant="contained">Edit</Button>
                     <Button
--- client/src/components/Invoice/InvoiceForm.jsx
+++ client/src/components/Invoice/InvoiceForm.jsx
@@ -1,21 +1,15 @@
-import React, {Fragment, useState, useEffect } from "react";
-import {
-  Alert,
-  Box,
-  FormControl,
-  Card,
-  Button,
-  FormHelperText,
-  TextField,
-  Grid,
-  Typography,
-  unstable_composeClasses,
-} from "@mui/material";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
-import Paper from "@mui/material/Paper";
-import OutlinedInput from "@mui/material/OutlinedInput";
+import {Grid, Typography} from "@mui/material";
+import {Container} from "@mui/system";
+import Paper from '@mui/material/Paper';
+import Select from '@mui/material/Select';
+import {useTheme} from '@mui/material/styles';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import {Fragment, useEffect, useState} from "react";
+import {TextField} from '@mui/material';
+import {Button} from '@mui/material';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -23,210 +17,203 @@ const MenuProps = {
   PaperProps: {
     style: {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
+      width: 250
+    }
+  }
 };
 
+function InvoiceForm({getOrders}) {
 
-function InvoiceForm() {
-  const [purchaseorder, setPurchaseorder] = React.useState("");
-  const [product, setProduct] = useState("");
-  const [vendor, setVendor] = useState("");
-  const [amount, setAmount] = useState([]);
-  const [quantity, setQuantity] = useState("");
-  const [accountname, setAccountname] = useState("");
-  const [errors, setErrors] = useState([]);
-
-  const [purchaseorders, setPurchaseorders] = useState([]);
-  const handleChange = (event) => {
-    setPurchaseorder(event.target.value);
-  };
-
-  const [products, setProducts] = useState([]);
-  const handleChangeProduct = (event) => {
-    setProduct(event.target.value);
-  };
-
-  const [vendors, setVendors] = useState([]);
-  const handleChangeVendor = (event) => {
-    setVendors(event.target.value);
-  };
-
-  // fetches categories
+  // fetchproducts
+  const [products,
+    setProducts] = useState([])
   useEffect(() => {
-    fetch("http://localhost:3000/purchaseorders")
-      .then((r) => r.json())
-      .then((data) => setPurchaseorders(data));
-  }, []);
-
-  // fetches products
+    fetch("http://localhost:3000/products").then((r) => r.json()).then((products) => {
+      setProducts(products)
+    })
+  }, [])
+  //fetch vendors
+  const [vendors,
+    setVendors] = useState([])
   useEffect(() => {
-    fetch("http://localhost:3000/products")
-      .then((r) => r.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  // fetches vendors
+    fetch("http://localhost:3000/vendors").then((r) => r.json()).then((vendors) => {
+      setVendors(vendors)
+    })
+  }, [])
+  //fetch invoices
+  const [invoices,
+    setInvoices] = useState([])
   useEffect(() => {
-    fetch("http://localhost:3000/vendors")
-      .then((r) => r.json())
-      .then((data) => setVendors(data));
-  }, []);
+    fetch("http://localhost:3000/invoices").then((r) => r.json()).then((invoices) => {
+      setInvoices(invoices)
 
-  function handleOnSubmit(e) {
+    })
+  }, [])
+
+  const [purchaseorders,
+    setPurchaseOrders] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3000/purchaseorders").then((r) => r.json()).then((purchase) => {
+      setPurchaseOrders(purchase)
+
+    })
+  }, [])
+
+  const [orders,
+    setOrders] = useState({
+    product_id: "",
+    vendor_id: "",
+    purchaseorder_id: "",
+    quantity: "",
+    amount: "",
+    accountname: ""
+  })
+  function onDataChange(e) {
+    setOrders({
+      ...orders,
+      [e.target.name]: e.target.value
+    });
+  }
+  function handleSubmit(e) {
     e.preventDefault();
+    const createdOrders = {
+      product_id: orders.product_id,
+      vendor_id: orders.vendor_id,
+      purchaseorder_id: orders.purchaseorder_id,
+      quantity: orders.quantity,
+      amount: orders.amount,
+      accountname: orders.accountname
+
+    };
+
     fetch("http://localhost:3000/invoices", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify({
-        accountname,
-        amount,
-        quantity,
+      body: JSON.stringify(createdOrders)
+    }).then((res) => res.json()).then((newOrder) => {
+      getOrders(newOrder);
+      setOrders({
+        ...orders,
+        product_id: "",
+        vendor_id: "",
+        purchaseorder_id: "",
+        quantity: "",
+        amount: "",
+        accountname: ""
+      });
+    });
 
-        // description,
-        product,
-        vendor,
-        purchaseorder,
-      }),
-    })
-      .then((r) => r.json())
-      .then((response) => setProduct(response));
-    // .then((response) => console.log(response));
   }
 
   return (
     <Fragment>
       <Grid container spacing={10}>
         {/* Chart */}
-        <Grid
-          item
-          xs={12}
-          md={8}
-          lg={9}
-          sx={{
-            ml: 50,
-          }}
-        >
+        <Grid item xs={12} md={8} lg={9} sx={{
+          ml: 50
+        }}>
           <Paper
             sx={{
-              p: 2,
-              display: "flex",
-              flexDirection: "column",
-              height: 650,
-            }}
-          >
+            p: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            height: 600,
+
+          }}>
             <Typography variant="h4" gutterBottom>
-              Create Invoice
+              Create Invoices
             </Typography>
+            <FormControl sx={{
+              m: 1,
+              width: 500
+            }}>
+              <InputLabel >Product</InputLabel>
+              <Select name="product_id" onChange={onDataChange}>
+                {products.map((product) => (
+                  <MenuItem key={product.id} value={product.id}>
+                    {product.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <FormControl sx={{
+              m: 1,
+              width: 500
+            }}>
+              <InputLabel >Vendor</InputLabel>
+              <Select name="vendor_id" onChange={onDataChange}>
+                {vendors.map((vendor) => (
+                  <MenuItem key={vendor.id} value={vendor.id}>
+                    {vendor.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <FormControl sx={{
+              m: 1,
+              width: 500
+            }}>
+              <InputLabel>PO Number</InputLabel>
+              <Select
+                input={< OutlinedInput label = "PurchaseOrder" />}
+                name="purchaseorder_id"
+                onChange={onDataChange}>
+                {purchaseorders.map((purchaseorder) => (
+                  <MenuItem key={purchaseorder.id} value={purchaseorder.id}>
+                    {purchaseorder.id}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
             <TextField
-              id="accountname"
-              label="Enter Account Name"
-              variant="outlined"
-              name="account"
-              value={accountname}
-              onChange={(e) => setAccountname(e.target.value)}
-              sx={{ width: 500, m: 1 }}
-            />
-            <TextField
-              id="amount"
-              label="Enter Amount"
-              variant="outlined"
-              name="amount"
-              value={amount}
-              sx={{ width: 500, m: 1 }}
-              onChange={(e) => setAmount(e.target.value)}
-            />
-            <TextField
-              id="quantity"
-              label="Enter Quantity"
+              id="outlined-basic"
+              label="Quantity"
               variant="outlined"
               name="quantity"
-              value={quantity}
-              sx={{ width: 500, m: 1 }}
-              onChange={(e) => setQuantity(e.target.value)}
-            />
-            <FormControl
+              value={orders.quantity}
+              onChange={onDataChange}
               sx={{
-                m: 1,
-                width: 500,
-              }}
-            >
-              <InputLabel>Purchase Order</InputLabel>
-              <Select
-                // input={<OutlinedInput label="Invoice" />}
-                name="invoice_id"
-                onChange={handleChange}
-                value={purchaseorder}
-                label="Purchase Order"
-              >
-                {purchaseorders.map((item) => (
-                  <MenuItem key={item.id} value={item.id}>
-                    {item.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-
-            <FormControl
+              width: 500,
+              m: 1
+            }}/>
+            <TextField
+              id="outlined-basic"
+              label="Amount"
+              variant="outlined"
+              name="amount"
+              value={orders.amount}
+              onChange={onDataChange}
               sx={{
-                m: 1,
-                width: 500,
-              }}
-            >
-              <InputLabel>Products</InputLabel>
-              <Select
-                // input={<OutlinedInput label="Invoice" />}
-                name="invoice_id"
-                onChange={handleChangeProduct}
-                value={product}
-                label="Product"
-              >
-                {products.map((item) => (
-                  <MenuItem key={item.id} value={item.id}>
-                    {item.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-
-            <FormControl
+              width: 500,
+              m: 1
+            }}/>
+            <TextField
+              id="outlined-basic"
+              label="Quantity"
+              variant="outlined"
+              name="accountname"
+              value={orders.accountname}
+              onChange={onDataChange}
               sx={{
-                m: 1,
-                width: 500,
-              }}
-            >
-              <InputLabel>Vendor</InputLabel>
-              <Select
-                // input={<OutlinedInput label="Invoice" />}
-                name="invoice_id"
-                onChange={handleChangeVendor}
-                value={vendor}
-                label="Vendor"
-              >
-                {vendors.map((item) => (
-                  <MenuItem key={item.id} value={item.id}>
-                    {item.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-
+              width: 500,
+              m: 1
+            }}/>
             <Button
               variant="contained"
               type="submit"
-              sx={{ mt: 2 }}
-              onClick={handleOnSubmit}
-            >
-              Submit
-            </Button>
+              sx={{
+              mt: 2
+            }}
+              onClick={handleSubmit}>Submit</Button>
           </Paper>
         </Grid>
       </Grid>
     </Fragment>
-  );
-}
+  )
 
-export default InvoiceForm;
+}
+export default InvoiceForm
